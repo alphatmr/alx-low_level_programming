d8cbe4b857fbf751d881a7e57d6c135cdb0e9b73
alphatmr <alphatmr27@gmail.com>
2022-12-08 10:52:59 -0800

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Starting Point in C
+ *
+ * Return: 0 indicates no error
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art, \n");
+	return (0);
+
+}
