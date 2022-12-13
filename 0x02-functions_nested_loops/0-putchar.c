8c84d932dce6f526b702ef2a71dfdcb2d0da2bd8
alphatmr <alphatmr27@gmail.com>
2022-12-13 13:09:27 -0800

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main -  program that prints _putchar, followed by a new line.
+ *
+ * Return: 0 indicates success
+ */
+
+int main()
+{
+	printf("_putchar");
+
+	printf("\n");
+
+	return (0);
+}
