84f30185e22d79c48be5e070e52cb33b4debba45
alphatmr <alphatmr27@gmail.com>
2022-12-14 05:24:20 -0800

    readme and main.c loops and function

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,3 @@
-#ifndef MAIN_H
-#define MAIN_H
-
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -11,3 +8,5 @@ int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
+int add(int, int);
+void print_to_98(int n);
