352bc89b253f14e0328ccc6c143ff299b0b6a037
alphatmr <alphatmr27@gmail.com>
2022-12-14 06:00:42 -0800

    a function that prints the alphabet, in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *main - a function that prints the alphabet
+ *
+ * Return: always 0
+ */
+
+void print_alphabet(void)
+{
+	char str;
+
+	for (str = 'a'; str <= 'z'; str++)
+		_putchar(str);
+
+	_putchar('\n');
+
+	return (0);
+
+}
