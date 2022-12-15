a810c85352da275d9f8842091534c2db65b02137
alphatmr <alphatmr27@gmail.com>
2022-12-15 04:10:31 -0800

    print_most_numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *
+ * print_most_numbers - Write a function that prints most of the numbers
+ *
+ * Return: from 0 to 9 followed by a new line.
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48; i < 58; a++)
+	{
+		if ((a != 50) || (a != 52))
+		{
+			_putchar(i);
+		}
+	}
+
+	_putchar('\n');
+
+}
