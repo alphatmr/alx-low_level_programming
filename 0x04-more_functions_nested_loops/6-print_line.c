0d3643870b33ccc27a86e1de2dd7ddd964e3f5a5
alphatmr <alphatmr27@gmail.com>
2022-12-15 07:54:49 -0800

    print_line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -14,13 +14,13 @@ void print_line(int n)
 
 	if (n <= 0)
 	{
-		_putchar('\n')
+		_putchar('\n');
 
 	}
 
 	else
 	{
-		for(sl = 1; sl <= n; sl++)
+		for (sl = 1; sl <= n; sl++)
 		{
 			_putchar('_');
 		}
