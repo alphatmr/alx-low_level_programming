f37c77b722b5db238222d247051adbfecd89ed64
alphatmr <alphatmr27@gmail.com>
2022-12-15 04:27:00 -0800

    4-print_most_numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,11 +10,11 @@ void print_most_numbers(void)
 {
 	int i;
 
-	for (i = 48; i < 58; a++)
+	for (i = 0; i <= 9; i++)
 	{
-		if (i != 50)
+		if (i != 2)
 		{
-			if (i != 52)
+			if (i != 4)
 			{
 				_putchar(i);
 			}
