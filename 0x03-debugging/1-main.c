981be2a6cf284643da7d99927c9de4fd6211854b
alphatmr <alphatmr27@gmail.com>
2022-12-16 03:05:07 -0800

    1-main

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ *
+ * main - identify the cause for infinite loops
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i;
+
+	printf("Infinite loop incoming :(\n");
+
+	i = 0;
+
+	/* while (i < 10)*/
+	/* {*/
+	/* putchar(i);*/
+	/* }*/
+
+	printf("Infinite loop avoided! \\o/\n");
+
+	return (0);
+
+}
