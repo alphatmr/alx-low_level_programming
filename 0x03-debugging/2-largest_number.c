5694b31bc284772c99c3a03058684dcc1bd13b0e
alphatmr <alphatmr27@gmail.com>
2022-12-16 04:39:08 -0800

    2-largest_number

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -15,13 +15,19 @@ int largest_number(int a, int b, int c)
 	int largest;
 
 	if (a >= b && a >= c)
+	{
 		largest = a;
+	}
 
 	else if (b >= c && b >= a)
+	{
 		largest = b;
+	}
 
 	else
+	{
 		largest = c;
+	}
 
 	return (largest);
 
