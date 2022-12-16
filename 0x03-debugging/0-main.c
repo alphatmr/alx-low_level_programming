e261bde00f647b25b84cb6397be05d568d34daa7
alphatmr <alphatmr27@gmail.com>
2022-12-16 02:10:48 -0800

    positive_or_negative

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *
+ * main - test that the function positive_or_negative() gives the correct output
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+
+	positive_or_negative();
+
+	return (0);
+
+}
