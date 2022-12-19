3e0fa6565ab65c828213c2b0559e35fa4ebd330a
alphatmr <alphatmr27@gmail.com>
2022-12-19 11:58:28 -0800

    strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * *_strcpy - a function that copies the string pointed.
+ * @dest: copy value to.
+ * @src: copy value from.
+ * Return: string value.
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	for (; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	return (dust);
+
+}
